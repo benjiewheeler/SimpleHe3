cc7cf2b63280126d82336535d8c04f6b504d2b39
Benjamin Wheeler <benjiewheeler@yandex.com>
2022-07-01 23:03:18 +0100

    fix: add z-index to Alert component

--- src/Components/Alert.tsx
+++ src/Components/Alert.tsx
@@ -4,7 +4,7 @@ export function Alert(props: { color: string; message: string }) {
 	return (
 		<>
 			{props?.message && (
-				<div className={`flex flex-col p-4 rounded drop-shadow pointer fixed top-5 right-5 bg-${props.color}`}>
+				<div className={`z-50 flex flex-col p-4 rounded drop-shadow pointer fixed top-5 right-5 bg-${props.color}`}>
 					<span className="text-white text-xm">{props.message}</span>
 				</div>
 			)}
