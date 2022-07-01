679ab7e738b7975f70800dbdeb9c5795c4a288f5
Benjamin Wheeler <benjiewheeler@yandex.com>
2022-07-01 22:07:13 +0100

    fix: re-organize topbar

--- src/Components/TopBar.tsx
+++ src/Components/TopBar.tsx
@@ -31,54 +31,59 @@ export function TopBar() {
 	return (
 		<div className="bg-gray-700 flex flex-col">
 			<div className="p-2 flex flex-row flex-wrap">
-				<div className="flex flex-col justify-center mr-1">
-					<span className="text-white text-xl font-bold">SimpleHe3</span>
-				</div>
-				<div className="flex flex-row flex-grow self-center justify-center mx-1">
-					<span className="text-white text-sm">{ual?.activeUser?.accountName}</span>
+				<div className="p-2 flex flex-row flex-wrap flex-grow justify-between">
+					<div className="flex flex-col justify-center mr-1">
+						<span className="text-white text-xl font-bold">SimpleHe3</span>
+					</div>
+
+					<div className="flex flex-row self-center justify-center mx-1">
+						<span className="text-white text-sm">{ual?.activeUser?.accountName}</span>
+					</div>
 				</div>
 
-				<div className="flex flex-col mx-1">
-					<select
-						className="bg-transparent border border-gray-500 rounded text-gray-400 text-xs font-mono mb-1"
-						value={waxEndpoint}
-						onChange={e => saveWAXEndpoint(e.target.value)}
-					>
-						{ENDPOINTS.API.map(endpoint => (
-							<option className="text-black" key={endpoint} value={endpoint}>
-								{endpoint}
-							</option>
-						))}
-					</select>
-					<select
-						className="bg-transparent border border-gray-500 rounded text-gray-400 text-xs font-mono"
-						value={atomicEndpoint}
-						onChange={e => saveAtomicEndpoint(e.target.value)}
-					>
-						{ENDPOINTS.ATOMIC.map(endpoint => (
-							<option className="text-black" key={endpoint} value={endpoint}>
-								{endpoint}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="flex flex-row ml-1">
-					{!ual.activeUser && (
-						<button
-							className="text-sm text-black bg-yellow-600 p-1 rounded hover:bg-yellow-800 hover:text-white"
-							onClick={() => ual.showModal()}
+				<div className="flex flex-row flex-wrap justify-between flex-grow md:flex-grow-0">
+					<div className="flex flex-col mx-1">
+						<select
+							className="bg-transparent border border-gray-500 rounded text-gray-400 text-xs font-mono mb-1"
+							value={waxEndpoint}
+							onChange={e => saveWAXEndpoint(e.target.value)}
 						>
-							Login
-						</button>
-					)}
-					{ual.activeUser && (
-						<button
-							className="text-sm text-black bg-slate-600 p-1 rounded hover:bg-slate-800 hover:text-white"
-							onClick={() => ual.logout()}
+							{ENDPOINTS.API.map(endpoint => (
+								<option className="text-black" key={endpoint} value={endpoint}>
+									{endpoint}
+								</option>
+							))}
+						</select>
+						<select
+							className="bg-transparent border border-gray-500 rounded text-gray-400 text-xs font-mono"
+							value={atomicEndpoint}
+							onChange={e => saveAtomicEndpoint(e.target.value)}
 						>
-							Logout
-						</button>
-					)}
+							{ENDPOINTS.ATOMIC.map(endpoint => (
+								<option className="text-black" key={endpoint} value={endpoint}>
+									{endpoint}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className="flex flex-row ml-1">
+						{!ual.activeUser && (
+							<button
+								className="text-sm text-black bg-yellow-600 p-1 rounded hover:bg-yellow-800 hover:text-white"
+								onClick={() => ual.showModal()}
+							>
+								Login
+							</button>
+						)}
+						{ual.activeUser && (
+							<button
+								className="text-sm text-black bg-slate-600 p-1 rounded hover:bg-slate-800 hover:text-white"
+								onClick={() => ual.logout()}
+							>
+								Logout
+							</button>
+						)}
+					</div>
 				</div>
 			</div>
 			{ual?.activeUser && (
