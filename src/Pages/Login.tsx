6224a490fab425fb8908981d7abec31d96eb5a66
Benjamin Wheeler <benjiewheeler@yandex.com>
2022-07-02 17:39:12 +0100

    docs: add github link
    update the disclaimer
    fix the external link icon

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -14,23 +14,28 @@ export function Login(): JSX.Element {
 					<p className="text-sm my-2">
 						This website is <span className="font-bold">NOT</span> the official UI of{" "}
 						<a className="text-violet-600" href="https://mmhe3.io/" target="_blank" rel="nofollow">
-							Moon Mining He3 &#x1F855;
+							Moon Mining He3 &#8599;
 						</a>
 					</p>
 					<p className="text-sm my-2">
-						This website is developed by <span className="font-bold text-indigo-500">Benjie#5458</span> for personal use (on mobile), and is not affiliated with Moon Mining He3
+						This website is developed by <span className="font-bold text-indigo-500">Benjie#5458</span> for personal use (on
+						mobile), and is not affiliated with Moon Mining He3
 					</p>
 					<p className="text-sm my-2">Not all the features of MMHe3 might be implemented, You'll have to use the official UI</p>
 					<p className="text-sm my-2">
 						This website is hosted for free, but if you want to buy me a{" "}
 						<a className="italic text-blue-600" target="_blank" rel="nofollow" href="https://wax.bloks.io/account/benjiewaxbag">
-							coffee
+							coffee &#8599;
 						</a>{" "}
 						I wouldn't stop you ;)
 					</p>
 					<p className="text-sm my-2">
-						Happy Mining
+						The source code is publicly available on{" "}
+						<a className="font-bold" target="_blank" rel="nofollow" href="https://github.com/benjiewheeler/SimpleHe3">
+							Github &#8599;
+						</a>
 					</p>
+					<p className="text-sm my-2">Happy Mining</p>
 				</div>
 			</div>
 		</>
